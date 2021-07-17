ba9282ae614c434079db5abdf79516e28339947b
Jeffrey Yeh <nightcrow33@gmail.com>
2021-07-17 06:09:40 -0400

    modify download button animation

--- src/components/Matlab.vue
+++ src/components/Matlab.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container">
-    <!-- <a href="/Matlab/order1000_.mat" download>test</a> -->
-    <select v-model="selected">
+    <span class="filetype">Matlab files</span>
+    <select v-model="selected" class="list">
       <option v-for="item in List" :value="item" :key="'matlab' + item">
         {{item}}
       </option>
     </select>
-    <span>{{selected}}</span>
-    <a :href="'/Matlab/order' + selected + '_.mat'" @click="download">Download</a>
+    <a :href="'/Matlab/order' + selected + '_.mat'" @click="download">
+      <button class="download">
+        Download
+      </button>
+    </a>
   </div>
 </template>
 
@@ -19,8 +22,29 @@
     data () {
       return {
         List: List,
-        selected: ''
+        selected: '1'
       }
     }
   }
 </script>
+
+<style>
+  .filetype {
+    font-size: 1em;
+    font-weight: bold;
+  }
+  .list {
+    margin-left: 1em;
+    margin-right: 1em;
+  }
+  .download {
+    background: white;
+    width: 7em;
+    box-shadow: inset 0 0 0 0 black;
+    transition: ease 0.5s;
+  }
+  .download:hover {
+    box-shadow: inset 7em 0 0 0 black;
+    color: white;
+  }
+</style>
