b2ef7a9a77b9a6d4c919595e673a64227a7b38c2
Jeffrey Yeh <nightcrow33@gmail.com>
2021-07-17 12:21:27 -0400

    center github link in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="footer">
+    <div class="container">
+      <a class="repo" href="https://github.com/TsungLinYeh/a-golay-hadamard-unitary" target="_blank">
+          check on github
+      </a>
+    </div>
   </div>
 </template>
 
@@ -12,9 +17,21 @@
 <style>
   .footer {
     position: absolute;
+    display: flex;
     bottom: 0;
     height: 5vh;
     width: 100vw;
     background: black;
+    justify-content: center;
+  }
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .repo {
+    font-size: 0.5em;
+    color: white;
+    text-decoration: none;
   }
 </style>
